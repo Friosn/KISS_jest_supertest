5c6f1f83c2cf823582d4d41eff5d7d2d6fbf4d28
Friosn <franz.rios.nino@gmail.com>
2023-01-26 18:31:56 +0100

    Connection made, server running and linked to DB

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const mongoose = requiere("mongoose");
+const mongoose = require("mongoose");
+const createServer = require("./server");
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(
+    "mongodb+srv://root:root@cluster0.vskhknf.mongodb.net/testing?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    const app = createServer();
+    app.listen(PORT, () => {
+      console.log("This server has started 🚀 🌕");
+    });
+  });
