f3d9cc88fc522f3a0b372fbeaa46a3aa4c49c89d
Friosn <franz.rios.nino@gmail.com>
2023-01-26 17:44:55 +0100

    CRUD controller done

--- src/api/controller/post.controller.js
+++ src/api/controller/post.controller.js
@@ -31,3 +31,27 @@ const postOne = async (req, res, next) => {
     return next(404);
   }
 };
+
+const patchOne = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const model = new Model(req.body);
+    model._id = id;
+    const updateModel = await Model.findByIdAndUpdate(id);
+    res.status(201).json(updateModel);
+  } catch (error) {
+    return next(404);
+  }
+};
+
+const deleteOne = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const model = await Model.findByIdAndDelete(id);
+    res.status(201).json(model);
+  } catch (error) {
+    return next(404);
+  }
+};
+
+module.exports = { getAll, getOne, postOne, patchOne, deleteOne };
