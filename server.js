95b60ec06ac643e1f78d0592ea3e6f84a376fd65
Friosn <franz.rios.nino@gmail.com>
2023-01-26 18:06:16 +0100

    Feat: server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const Router = require("./src/api/routes/post.routes");
+
+function createServer() {
+  const app = express();
+  app.use(express.json());
+  app.use("/api", Router);
+  return app;
+}
+
+module.exports = createServer;
