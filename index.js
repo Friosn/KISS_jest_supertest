b4cb7583093877dcbf8d815d5515c5c0f20c0ade
Friosn <franz.rios.nino@gmail.com>
2023-02-07 20:56:52 +0100

    MONGO_URI deleted

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const mongoose = require("mongoose");
 const createServer = require("./server");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose
-  .connect(
-    "mongodb+srv://root:root@cluster0.vskhknf.mongodb.net/testing?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    const app = createServer();
-    app.listen(PORT, () => {
-      console.log("This server has started 🚀 🌕");
-    });
+mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(() => {
+  const app = createServer();
+  app.listen(PORT, () => {
+    console.log("This server has started 🚀 🌕");
   });
+});
